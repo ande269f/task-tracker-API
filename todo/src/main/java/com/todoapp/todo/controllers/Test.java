e8da58e7e55a2865ebb10231637b5429c66fe93a
ande269f <andersknudsen98@gmail.com>
2025-09-03 13:51:40 +0200

    i gang med at sætte det hele op - skal genstarte så det bliver et hutigt commit

---
+++ todo/src/main/java/com/todoapp/todo/controllers/Test.java
@@ -0,0 +1,8 @@
+package com.todoapp.todo.controllers;
+import org.springframework.web.bind.annotation.RestController;
+import java.sql.DriverManager;
+
+@RestController
+public class Test {
+    
+}
